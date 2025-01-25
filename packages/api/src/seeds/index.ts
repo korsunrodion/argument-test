60e27cc559fd86d74650cbb158bac34af51bfd7b
Rodion <korsun.rodion@gmail.com>
2025-01-25 10:28:40 +0200

    fix indent

--- packages/api/src/seeds/index.ts
+++ packages/api/src/seeds/index.ts
@@ -3,19 +3,19 @@ import { EarthquakeSeeder } from "../seeds/Earthquake.seeder"
 import { AppDataSource } from "../data-source";
 
 async function main() {
-    try {
-        await AppDataSource.initialize();
-        console.log("Running seeds...");
-        
-        const earthquakeSeeder = new EarthquakeSeeder();
-        await earthquakeSeeder.run(AppDataSource);
-        
-        console.log("Seeds executed successfully");
-        process.exit(0);
-    } catch (error) {
-        console.error("Error during seeding:", error);
-        process.exit(1);
-    }
+  try {
+    await AppDataSource.initialize();
+    console.log("Running seeds...");
+    
+    const earthquakeSeeder = new EarthquakeSeeder();
+    await earthquakeSeeder.run(AppDataSource);
+    
+    console.log("Seeds executed successfully");
+    process.exit(0);
+  } catch (error) {
+    console.error("Error during seeding:", error);
+    process.exit(1);
+  }
 }
 
 main();
